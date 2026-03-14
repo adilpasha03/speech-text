cb0ef099d293e33fabf24f411b13d7123e619dc7
Adil Pasha <adilp6195@gmail.com>
2026-03-14 17:03:43 +0530

    https://backend-vtvz.onrender.com

--- backend/server.js
+++ backend/server.js
@@ -148,10 +148,8 @@ app.delete("/delete/:id", async (req, res) => {
 
 
 // Server start
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(`Server running on port ${PORT}`)
-
-})
+  console.log(`Server running on port ${PORT}`);
+});
